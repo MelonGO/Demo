e1bf62182c4f5f8c27cce9e061398388f9feee8f
MelonGO <cjwone1@163.com>
2016-11-28 14:50:03 +0800

    # WARNING: head commit changed in the meantime
    initial

---
+++ src/main/java/com/major/dao/UserDAO.java
@@ -0,0 +1,36 @@
+package com.major.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.StatementType;
+import org.mybatis.spring.annotation.MapperScan;
+
+import com.major.dao.constants.UserDaoConstants;
+import com.major.model.User;
+
+@MapperScan
+public interface UserDAO {
+   
+
+    @Insert({"insert into ", UserDaoConstants.TABLE_NAME, "(", UserDaoConstants.INSERT_FIELDS,
+            ") values (#{name},#{password},#{role})"})
+    @SelectKey(statement="select last_insert_id() as id", keyProperty="id", before=false, resultType=Integer.class,
+            statementType = StatementType.PREPARED)
+    int addUser(User user);
+
+    @Select({"select ", UserDaoConstants.SELECT_FIELDS, " from ", UserDaoConstants.TABLE_NAME, " where id=#{id}"})
+    User selectById(int id);
+
+    @Select({"select ", UserDaoConstants.SELECT_FIELDS, " from ", UserDaoConstants.TABLE_NAME, " where name=#{name}"})
+    User selectByName(String name);
+
+    @Update({"update ", UserDaoConstants.TABLE_NAME, " set password=#{password} where id=#{id}"})
+    void updatePassword(User user);
+
+    @Delete({"delete from ", UserDaoConstants.TABLE_NAME, " where id=#{id}"})
+    void deleteById(int id);
+    
+    @Select({"select ", UserDaoConstants.SELECT_FIELDS, " from ", UserDaoConstants.TABLE_NAME})
+    List<User> getAll();
+}
